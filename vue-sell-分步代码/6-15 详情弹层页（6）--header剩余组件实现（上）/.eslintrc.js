4c0d16c28f0555fd36c5f3f28c9c269a2be11eeb
theWindingPath <2958202145@qq.com>
2018-11-19 10:26:27 +0800

    6-15 详情弹层页（6）--header剩余组件实现（上）

---
+++ vue-sell-分步代码/6-15 详情弹层页（6）--header剩余组件实现（上）/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
+  extends: 'standard',
+  // required to lint *.vue files
+  plugins: [
+    'html'
+  ],
+  // add your custom rules here 在这里可以对ES6语法做自定义配置
+  'rules': {
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    // 增加自定义配置 semi 允许有分号，es6默认：有分号；会报错
+    'semi': ['error', 'always'], // 配置为没有分号会报错
+    'indent': 0,
+    'space-before-function-paren': 0
+  }
+}
